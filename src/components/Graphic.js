2fd991eefb3ae7fcd06f90525dd9f8464d874dbb
Casper Yonel <yonelcasper@gmail.com>
2021-11-30 20:54:17 -0700

    added dashboard

--- src/components/Graphic.js
+++ src/components/Graphic.js
@@ -1,8 +1,9 @@
-import dashboard from "../dummyDashboard.png"
+import dashboard from "../Solidity Dashboard (8).png"
 
 const Graphic = () => {
     return (
         <div className="block-2-right">
+            {/* <img id="dashboard" src={dashboard} alt="" /> */}
             <img id="dashboard" src={dashboard} alt="" />
         </div>
     )
